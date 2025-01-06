6f77dccda73497d3703eeabfe7df69f99ad417c6
Sujal-Jariwala <jariwalasujal68@gmail.com>
2025-01-06 17:03:43 +0530

    fix test: ios svg animation test

--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -8,16 +8,24 @@ export default function Skills(){
 
     const line1Ref = useRef<SVGPathElement|null>(null)
     const line2Ref = useRef<SVGPathElement|null>(null)
-
+    const wasInView = useRef(false)
+    
     useEffect(()=>{
+        
+        
         const observer = new IntersectionObserver((entries)=>{
+            // console.log(entries);
+            
             entries.forEach(entry=>{
-                const element = entry.target as HTMLElement
-                if (entry.isIntersecting) {
-                    entry.target.classList.add(element.dataset.animation || '')
+                if (entry.isIntersecting && !wasInView.current) {
+                    entry.target.classList.add('animate')
+                    console.log(entries[1].target.className);
+                    
+                    wasInView.current = true
                 }else{
-                    element.classList.remove(element.dataset.animation ||'')
-                }
+                    entry.target.classList.remove('animate')
+                    wasInView.current = false
+                }                
             })
         },{threshold:.1});
         if (line1Ref.current) {
@@ -33,10 +41,10 @@ export default function Skills(){
             <div className='skillsMain_components'>
                 <div className='container1'>
                     <p className='md'>My <svg className='line1' width="178" height="15" viewBox="0 0 178 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path ref={line1Ref} data-animation={'animateLine'} d="M4.2007 4.59502C60.8452 4.23423 117.456 3.57268 173.806 10.6165" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
+                    <motion.path ref={line1Ref} className={'animateLine'} d="M4.2007 4.59502C60.8452 4.23423 117.456 3.57268 173.806 10.6165" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
                     </svg>
                     <svg className='line2' width="191" height="13" viewBox="0 0 191 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path ref={line2Ref} data-animation={'animateLine2'} d="M4.28312 5.65953C2.46813 4.99085 8.14772 5.39874 10.0816 5.43651C30.4849 5.83502 50.907 4.5129 71.3002 4.0984C110.006 3.3117 148.469 4.29906 186.935 8.67028" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
+                    <motion.path ref={line2Ref} className={'animateLine2'} d="M4.28312 5.65953C2.46813 4.99085 8.14772 5.39874 10.0816 5.43651C30.4849 5.83502 50.907 4.5129 71.3002 4.0984C110.006 3.3117 148.469 4.29906 186.935 8.67028" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
                     </svg>
                     <strong>Skills</strong></p>
                 </div>
