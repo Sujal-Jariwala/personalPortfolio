903e5ddeb617e2c8cec6e740c7813eebc8dbecc3
Sujal-Jariwala <jariwalasujal68@gmail.com>
2025-01-06 17:26:04 +0530

    update : let the svg animations be

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,16 +5,14 @@ import { useEffect, useRef } from 'react'
 
 export default function About(){
     const ovalRef = useRef<SVGPathElement|null>(null)
-    const inViewRef = useRef(false)
     useEffect(()=>{
         const observer = new IntersectionObserver((entries)=>{
             entries.forEach(entry=>{
-                if(entry.isIntersecting && !inViewRef.current){
-                    entry.target.classList.add('animate')
-                    inViewRef.current = true
+                const element = entry.target as HTMLElement
+                if(entry.isIntersecting){
+                    entry.target.classList.add(element.dataset.animate ||'')
                 }else{
-                    entry.target.classList.remove('animate')
-                    inViewRef.current = false
+                    entry.target.classList.remove(element.dataset.animate||'')
                 }
             })
         },{threshold:.1})
@@ -31,7 +29,7 @@ export default function About(){
                 </div>
                 <div className='rightSection'>
                     <p className='me'><svg width="291" height="94" viewBox="0 0 291 94" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path ref={ovalRef} className={'ovalAnimation'} d="M136.541 6.31906C131.977 2.02292 121.413 4.31189 115.689 4.31189C103.117 4.31189 90.29 3.44227 77.8873 5.81727C57.1983 9.779 31.5845 10.656 14.327 24.3836C7.11014 30.1242 7.18277 38.0589 5.07168 46.2394C2.59123 55.8511 3.25795 66.3355 12.2083 72.0538C21.8579 78.2188 36.2451 82.2186 47.4452 84.5986C53.1004 85.8003 58.631 85.2746 64.3389 85.7137C75.6178 86.5813 86.4585 89.3198 97.9032 89.5608C121.645 90.0606 145.331 89.2847 169.046 88.6687C187.474 88.1901 206.267 88.8411 224.634 87.3863C242.981 85.9331 273.55 85.4789 284.514 67.5377C288.62 60.8186 287.816 49.6916 284.57 42.9499C278.579 30.5075 267.533 26.6613 254.741 24.1606C232.355 19.7844 208.867 14.977 186.163 12.9539C152.645 9.96716 118.922 8.03688 85.3584 5.31548" stroke="#FFC000" strokeWidth="7" strokeLinecap="round"/>
+                    <motion.path ref={ovalRef} data-animate={'ovalAnimation'} d="M136.541 6.31906C131.977 2.02292 121.413 4.31189 115.689 4.31189C103.117 4.31189 90.29 3.44227 77.8873 5.81727C57.1983 9.779 31.5845 10.656 14.327 24.3836C7.11014 30.1242 7.18277 38.0589 5.07168 46.2394C2.59123 55.8511 3.25795 66.3355 12.2083 72.0538C21.8579 78.2188 36.2451 82.2186 47.4452 84.5986C53.1004 85.8003 58.631 85.2746 64.3389 85.7137C75.6178 86.5813 86.4585 89.3198 97.9032 89.5608C121.645 90.0606 145.331 89.2847 169.046 88.6687C187.474 88.1901 206.267 88.8411 224.634 87.3863C242.981 85.9331 273.55 85.4789 284.514 67.5377C288.62 60.8186 287.816 49.6916 284.57 42.9499C278.579 30.5075 267.533 26.6613 254.741 24.1606C232.355 19.7844 208.867 14.977 186.163 12.9539C152.645 9.96716 118.922 8.03688 85.3584 5.31548" stroke="#FFC000" strokeWidth="7" strokeLinecap="round"/>
                     </svg>About <strong>Me</strong></p>
                     <p className='p p1'>I'm a passionate, detail-oriented Frontend developer, I work in building user-friendly, dynamic web interfaces. With hands on in modern frameworks like ReactJs and styling tools such as Tailwind CSS and SCSS, I create visually appealing and responsive websites that enhance user experiences. User experience, pixel perfect design, and writing clear, maintainable, readable code matters to me.</p>
                     <p className='p p2'>I began my journey as a web developer in August 2024, and since then, I've continued to learn and challange myself by taking on new challanges and learning the latest technologies along the way. I'm currently 19 years of age and I make beautiful UI from designs using modern technologies like ReactJs, TypeScript, Tailwindcss, Scss and much more.</p>
--- src/components/Skills/Skills.tsx
+++ src/components/Skills/Skills.tsx
@@ -2,6 +2,7 @@ import './styles.scss'
 import { motion } from 'motion/react'
 import React, { useEffect, useRef } from 'react'
 import { skills } from '../../data'
+import { line } from 'motion/react-client'
 
 export default function Skills(){
 
@@ -9,13 +10,16 @@ export default function Skills(){
     const line2Ref = useRef<SVGPathElement|null>(null)
     
     useEffect(()=>{
-        const observer = new IntersectionObserver((entries)=>{            
+        
+        const observer = new IntersectionObserver((entries)=>{
+            // console.log(entries);
+            
             entries.forEach(entry=>{
+                const element = entry.target as HTMLElement
                 if (entry.isIntersecting) {
-                    entry.target.classList.add('animate')
-                    
+                    entry.target.classList.add(element.dataset.animate||'')                    
                 }else{
-                    entry.target.classList.remove('animate')
+                    entry.target.classList.remove(element.dataset.animate||'')
                 }                
             })
         },{threshold:.1});
@@ -32,10 +36,10 @@ export default function Skills(){
             <div className='skillsMain_components'>
                 <div className='container1'>
                     <p className='md'>My <svg className='line1' width="178" height="15" viewBox="0 0 178 15" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path ref={line1Ref} className={'animateLine'} d="M4.2007 4.59502C60.8452 4.23423 117.456 3.57268 173.806 10.6165" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
+                    <motion.path ref={line1Ref} data-animate={'animateLine'} d="M4.2007 4.59502C60.8452 4.23423 117.456 3.57268 173.806 10.6165" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
                     </svg>
                     <svg className='line2' width="191" height="13" viewBox="0 0 191 13" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <motion.path ref={line2Ref} className={'animateLine2'} d="M4.28312 5.65953C2.46813 4.99085 8.14772 5.39874 10.0816 5.43651C30.4849 5.83502 50.907 4.5129 71.3002 4.0984C110.006 3.3117 148.469 4.29906 186.935 8.67028" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
+                    <motion.path ref={line2Ref} data-animate={'animateLine1'} d="M4.28312 5.65953C2.46813 4.99085 8.14772 5.39874 10.0816 5.43651C30.4849 5.83502 50.907 4.5129 71.3002 4.0984C110.006 3.3117 148.469 4.29906 186.935 8.67028" stroke="#FFC000" stroke-width="7" stroke-linecap="round"/>
                     </svg>
                     <strong>Skills</strong></p>
                 </div>
