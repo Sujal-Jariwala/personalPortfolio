2a0e95b01f3055938e1bd9c709365b18d54cf72a
Sujal-Jariwala <jariwalasujal68@gmail.com>
2025-11-29 23:19:39 +0530

    some changes

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -92,7 +92,6 @@ const Contact = forwardRef<HTMLElement>((_,ref)=>{
                         <h2>Let's Talk for <br />Something Special !</h2>
                         <p>I seek to push the limits of creativity to create high-engaging, user-friendly, and memorable interactive experiences.</p>
                         <a href="mailto:jariwalasujal1415@gmail.com">jariwalasujal1415@gmail.com</a>
-                        <a href="tel:+91 6353952488">6353952488</a>
                     </div>
                 </div>
             </div>
